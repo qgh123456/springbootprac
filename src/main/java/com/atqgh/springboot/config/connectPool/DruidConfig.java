ad979784ee7c9fef7ca367ff920b2fa2f8bedb3c
qgh123456 <2104484868@qq.com>
2020-10-30 13:50:55 +0800

    新加的

--- src/main/java/com/atqgh/springboot/config/connectPool/DruidConfig.java
+++ src/main/java/com/atqgh/springboot/config/connectPool/DruidConfig.java
@@ -1,4 +1,4 @@
-package com.atqgh.springboot.config;
+package com.atqgh.springboot.config.connectPool;
 
 import com.alibaba.druid.pool.DruidDataSource;
 import com.alibaba.druid.support.http.StatViewServlet;
